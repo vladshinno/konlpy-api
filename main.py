a1c1ab5f2ef9833210c755fb7f966ffde455945e
vladshinno <vlad.shinno@gmail.com>
2026-05-29 10:39:38 +0300

    Enhance analyze function with API key check
    Added API key validation to analyze endpoint.

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, Header, HTTPException
 from konlpy.tag import Okt
 
 app = FastAPI()
 okt = Okt()
 
+SECRET = os.getenv("KONLPY_API_SECRET")
+
 @app.get("/analyze")
-def analyze(text: str):
-    return {"result": okt.pos(text)}
+def analyze(text: str, x_api_key: str = Header(None)):
+    if x_api_key != SECRET:
+        raise HTTPException(status_code=403)
+
+    return {
+        "morphs": okt.morphs(text)
+    }
